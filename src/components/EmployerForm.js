a7cb8142345bc80a40dd94bbb472bc210676d683
Firasu <lecters_app@hotmail.com>
2018-09-26 19:56:25 +0300

    try

--- src/components/EmployerForm.js
+++ src/components/EmployerForm.js
@@ -1,47 +1,53 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import { BrowserRouter as Router, Route, Link, withRouter } from "react-router-dom";
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import "../App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  withRouter
+} from "react-router-dom";
 
 import CandidateStore from "../stores/candidateStore";
-import EmployerStore from "../stores/employerStore";
-
-
 
 class EmployerForm extends Component {
-  render()
-  {
-    return(
-    <div className="App-third-column">
+  render() {
+    return (
+      <div className="App-third-column">
+        <form onSubmit={e => CandidateStore.submitcandidatemail(e)}>
+          <label>
+            Candidate Email:
+            <input
+              type="text"
+              onChange={e => (CandidateStore.candidateEmail = e.target.value)}
+            />
+          </label>
+          <br />
+          <br />
+          <label>
+            Company Email:
+            <input
+              type="text"
+              onChange={e => (CandidateStore.companyEmail = e.target.value)}
+            />
+          </label>
+          <br />
+          <br />
+          <label>
+            Company Name:
+            <input
+              type="text"
+              onChange={e => (CandidateStore.companyName = e.target.value)}
+            />
+          </label>
+          <br />
+          <br />
 
-      <form >
-      <label>
-      Candidate Email:
-      <input type="text" value={CandidateStore.setEmail(this.state.value)} />
-      </label>
-      <br></br>
-      <br></br>
-      <label>
-      Company Email:
-      <input type="text" value={EmployerStore.setEmail(this.state.value)} />
-      </label>
-      <br></br>
-      <br></br>
-      <label>
-      Company Name:
-      <input type="text" value={EmployerStore.setCompany(this.state.value)}/>
-      </label>
-      <br></br>
-      <br></br>
-
-      <input type="submit" value="Submit" />
-      <br></br>
-
-      </form>
-
-</div>
-    )
+          <input type="submit" value="Submit" />
+          <br />
+        </form>
+      </div>
+    );
   }
-
 }
 export default withRouter(EmployerForm);
--- src/stores/candidateStore.js
+++ src/stores/candidateStore.js
@@ -1,28 +1,51 @@
 import React from "react";
-import { decorate, observable, computed } from "mobx";
+import { decorate, observable, computed, action } from "mobx";
+import axios from "axios";
 
+const instance = axios.create({
+  baseURL: "https://127.0.0.1:8000"
+});
 
 class CandidateStore {
   constructor() {
     this.candidateEmail = "";
     this.candidates = [];
-
+    this.employerEmail = "";
+    this.employer = [];
+    this.employerCompany = "";
+    this.loading = true;
   }
 
+  submitcandidatemail(e) {
+    e.preventDefault();
 
+    let obj = {
+      candidate_email: this.candidateEmail,
+      company_name: this.companyName,
+      company_email: this.companyEmail
+    };
 
-  setEmail(email){
-    this.email = email
+    this.candidates.push(obj);
+
+    // instance
+    //   .post("/", obj)
+    //   .then(res => res.data)
+    //   .then(x => {
+    //     x = obj;
+    //   })
+    //   .catch(err => console.error(err));
   }
 }
 
 decorate(CandidateStore, {
   candidateEmail: observable,
+  employerEmail: observable,
+  employerCompany: observable,
   candidates: observable,
-
-
-
-
+  employer: observable,
+  setEmail: action,
+  setCompanyEmail: action,
+  setCompanyName: action
 });
 
 export default new CandidateStore();
