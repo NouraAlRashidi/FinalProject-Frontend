cf502327dde990bf21958f655bd36613eb4bfefb
Firasu <lecters_app@hotmail.com>
2018-09-27 19:32:52 +0300

    firasfront

---
+++ src/components/Test.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import testlogo from "../logo.svg";
+import "../App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  withRouter
+} from "react-router-dom";
+
+class Test extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+export default withRouter(Test);
